906e25d3bcf78da75a8d3df67779bb4c1629da95
NSi <root@devlab>
2014-10-23 12:11:24 +0200

    Update Gemfiles

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Heroku requires this to be false
+config.assets.initialize_on_precompile=false
+
 module FirstApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
